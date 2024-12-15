f06d38a13f4de1309d6dfc34820812606d1d7db2
root <ray.dotropg@gmail.com>
2024-12-15 15:02:41 +0000

    fixing betty of task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -25,7 +25,7 @@ void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 }
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ * insertion_sort_list - Sort doubly linked list of integer in ascending order
  * using the Insertion sort algorithm.
  *
  * @list: Pointer to the head of the list
